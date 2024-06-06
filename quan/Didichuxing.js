bcfae4f4e972d9f6f99b21d11a4b58f858e3a262
xdCheng <xdcheng52@xdChengdeMacBook-Pro.local>
2024-06-06 11:57:42 +0800

    chenge quan didi

--- quan/Didichuxing.js
+++ quan/Didichuxing.js
@@ -1,22 +1,43 @@
-// 2024-02-12 19:42
+// 2024-04-11 15:37
 const url = $request.url;
 if (!$response.body) $done({});
 
 let obj = JSON.parse($response.body);
 
-if (url.includes("/homepage/v1/core")) {
-  // 保留打车、顺风车、代驾、青桔骑行
-  const keepNavIds = ['dache_anycar', 'carmate', 'driverservice', 'bike'];
+if (url.includes("/other/pGetSceneList")) {
+  if (obj && obj.data && obj.data.scene_list instanceof Array) {
+    obj.data.scene_list = obj.data.scene_list.filter(item => item.text !== "优惠商城");
+  }
+  if (obj && obj.data && obj.data.show_data instanceof Array) {
+    obj.data.show_data.forEach((block) => {
+      if (block.scene_ids instanceof Array) {
+        block.scene_ids = block.scene_ids.filter(id => id !== "scene_coupon_mall");
+      }
+    });
+  }
+}
+
+if (url.includes("/homepage/")) {
+  // 保留打车、代驾、青桔骑行
+  const keepNavIds = ['dache_anycar', 'driverservice', 'bike' ];
   if (obj.data && obj.data.order_cards && obj.data.order_cards.nav_list_card && obj.data.order_cards.nav_list_card.data) {
     obj.data.order_cards.nav_list_card.data = obj.data.order_cards.nav_list_card.data.filter(item => keepNavIds.includes(item.nav_id));
   }
   // 保留底部tap首页、我的
-  const keepBottomNavIds = ['v6x_home', 'user_center'];
+  const keepBottomNavIds = ['v6x_home', 'user_center' ];
   if (obj.data && obj.data.disorder_cards && obj.data.disorder_cards.bottom_nav_list && obj.data.disorder_cards.bottom_nav_list.data) {
     obj.data.disorder_cards.bottom_nav_list.data = obj.data.disorder_cards.bottom_nav_list.data.filter(item => keepBottomNavIds.includes(item.id));
   }
 }
-if (url.includes("/usercenter/me")) {
+
+if (url.includes("/ota/na/yuantu/infoList")) {
+if (obj.data && obj.data.disorder_cards && obj.data.disorder_cards.top_banner_card && obj.data.disorder_cards.top_banner_card.data && obj.data.disorder_cards.top_banner_card.data[0] && obj.data.disorder_cards.top_banner_card.data[0].T === "yuentu_top_banner") {
+    // 移除顶部卡片
+    obj.data.disorder_cards.top_banner_card.data.splice(0, 1);
+ }
+}
+
+if (url.includes("/usercenter")) {
   const excludedTitles = ['天天领福利', '金融服务', '更多服务', '企业服务', '安全中心'];
 
   if (obj.data && obj.data.cards) {
